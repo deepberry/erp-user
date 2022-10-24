738d34ed2c5f7c2cc50c1c6096815a333f8bbd5b
mins <xu_xiangbo@163.com>
2022-10-24 10:00:46 +0800

    reset index

---
+++ src/views/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="index">
+        <!-- 头部菜单 -->
+        <IndexHeader></IndexHeader>
+
+        <!-- 侧边栏菜单 -->
+        <IndexMenu></IndexMenu>
+
+        <!-- 主内容区域 -->
+        <div class="main">
+            <router-view />
+        </div>
+    </div>
+</template>
+
+<script lang="js">
+import IndexHeader from "@/components/index/IndexHeader.vue";
+import IndexMenu from "@/components/index/IndexMenu.vue";
+export default {
+    name: "Index",
+    components: {
+        IndexHeader,
+        IndexMenu,
+    },
+    mounted (){
+        // 默认进入 智能设备 ied
+        if(this.$route.path == '/'){
+            this.$router.push('/ied');
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import url("@/assets/css/global.less");
+.index {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    .main {
+        width: calc(100% - 210px);
+        height: calc(100% - 70px);
+        position: absolute;
+        top: 70px;
+        left: 210px;
+        z-index: @indexMainLayer;
+    }
+}
+</style>
