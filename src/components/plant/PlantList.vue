21a4c34f906b90c7ef88c44860297a1e6a95c8e6
mins <xu_xiangbo@163.com>
2022-11-01 23:26:25 +0800

    作物-园区列表地

--- src/components/plant/PlantList.vue
+++ src/components/plant/PlantList.vue
@@ -1,3 +1,173 @@
 <template>
-    <div class="plantList">sss</div>
+    <div class="plantList">
+        <div class="list">
+            <div class="item" v-for="(item, index) in list" :key="index">
+                <div class="head">
+                    <div class="left">
+                        <img src="../../assets/img/ds.png" alt="" />
+                        <p>蓝莓</p>
+                    </div>
+                    <div class="right">第101天</div>
+                </div>
+                <div class="content">
+                    <div class="contentItem">
+                        <p><span>园区：</span>B区</p>
+                        <p><span>品种：</span>高丛蓝莓</p>
+                    </div>
+                    <div class="contentItem">
+                        <p><span>面积：</span>6.8亩</p>
+                        <p>2022.11.01 定值</p>
+                    </div>
+                </div>
+                <div class="btn">
+                    <p><i class="erp erpjiekouyunwei"></i> 智能设备</p>
+                    <p><i class="erp erprili"></i> 种植任务</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="pages" v-if="list.length > 0">
+            <span class="total">共 {{ total }} 条</span>
+            <el-pagination
+                v-model:currentPage="currentPage"
+                v-model:page-size="pageSize"
+                background
+                @current-change="getData"
+                layout="prev, pager, next, jumper"
+                :total="total"
+            />
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    name: "plantList",
+    data() {
+        return {
+            list: [1, 2, 3, 4, 5, 6, 7, 8],
+            currentPage: 1,
+            pageSize: 8,
+            total: 0,
+            loading: false,
+        };
+    },
+    mounted() {
+        // this.getData();
+    },
+    methods: {
+        // 获取列表
+        getData() {
+            this.loading = true;
+            this.ajax
+                .post("/api/v1/adam/adminGrowModel/list", {
+                    pageNum: this.currentPage,
+                    pageSize: 8,
+                    param: {
+                        id: 0,
+                        keyWord: "",
+                        state: 0,
+                    },
+                })
+                .then((r) => {
+                    this.list = r.data;
+                    this.total = r.total;
+                });
+        },
+    },
+};
+</script>
+
+<style lang="less" scoped>
+.plantList {
+    .list {
+        width: calc(100% - 360px);
+        padding: 40px 0;
+        margin-left: 160px;
+        display: flex;
+        justify-content: space-between;
+        align-content: flex-start;
+        flex-wrap: wrap;
+        .item {
+            width: 23%;
+            border: 1px solid #e4e4e4;
+            border-radius: 10px;
+            overflow: hidden;
+            margin-top: 50px;
+            .head {
+                height: 60px;
+                padding: 0 20px;
+                background: url("../../assets/img/1.png") no-repeat;
+                background-size: cover;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                .left {
+                    display: flex;
+                    justify-content: flex-start;
+                    align-items: center;
+                    img {
+                        width: 40px;
+                        height: 40px;
+                        border-radius: 50%;
+                    }
+                    p {
+                        margin-left: 10px;
+                        color: #232121;
+                        font-size: 16px;
+                        font-weight: bold;
+                    }
+                }
+            }
+            .content {
+                padding: 0 20px;
+                padding-bottom: 0;
+                .contentItem {
+                    margin-top: 30px;
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    p {
+                        color: #333333;
+                        span {
+                            color: #999999;
+                        }
+                    }
+                }
+            }
+            .btn {
+                width: calc(100% - 40px);
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                border-top: 1px solid #deedfa;
+                margin: 0 20px;
+                margin-top: 20px;
+                padding: 10px 0;
+                p {
+                    color: #1c82d7;
+                    width: 100%;
+                    cursor: pointer;
+                    text-align: center;
+                    i {
+                        margin-right: 3px;
+                    }
+                }
+                p:first-child {
+                    border-right: 1px solid #deedfa;
+                }
+            }
+        }
+    }
+    .pages {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 30px 0;
+        .total {
+            color: #a0a0a0;
+            margin-right: 10px;
+        }
+    }
+}
+</style>
--- src/views/erp/plant.vue
+++ src/views/erp/plant.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="stock inner">
-        <div class="box">
+        <div class="box" v-loading="loading">
             <div class="head">
                 <div class="headBox">
                     <div>
@@ -16,10 +16,50 @@
                 </div>
                 <router-view></router-view>
             </div>
-            <div class="btn">
+            <div class="btn" @click="showGardenList = true">
                 <i class="erp erpanniu_jiantouxiangzuo_o"></i>
                 <p>园区列表</p>
             </div>
+            <el-drawer
+                v-model="showGardenList"
+                :before-close="onCloseGardenList"
+                size="300px"
+                :show-close="false"
+                append-to-body
+            >
+                <div class="gardenList">
+                    <div class="head">
+                        <p @click="showGardenList = false">
+                            <i class="erp erpanniu_jiantouxiangzuo_o"></i>
+                            园区列表
+                        </p>
+                        <el-button type="primary">新增园区</el-button>
+                    </div>
+                    <div class="list">
+                        <div
+                            class="item"
+                            @click="showGardenList = false"
+                            v-for="(item, index) in gardenList"
+                            :key="index"
+                        >
+                            <img :src="item.listImage" alt="" />
+                            <div class="c">
+                                <p>{{ item.title }}</p>
+                                <p>蓝莓</p>
+                            </div>
+                            <i class="erp erpcaidan" @click.stop="item.showMenu = true"></i>
+                            <transition name="el-zoom-in-top">
+                                <div v-if="item.showMenu" class="menu" @mouseleave="item.showMenu = false">
+                                    <div class="menuPoint"></div>
+                                    <p @click.stop="">历史种植</p>
+                                    <p @click.stop="">编辑园区</p>
+                                    <p @click.stop="">删除园区</p>
+                                </div>
+                            </transition>
+                        </div>
+                    </div>
+                </div>
+            </el-drawer>
         </div>
     </div>
 </template>
@@ -36,6 +76,10 @@ export default {
                 },
             ],
             activeTabIndex: 0,
+            gardenList: [], // 园区列表
+            currentGarden: 0,
+            loading: false,
+            showGardenList: false,
         };
     },
     mounted() {
@@ -43,8 +87,31 @@ export default {
         if (this.$route.name == "erpPlant") {
             this.$router.push(this.headTab[0].path);
         }
+
+        this.getGardenList();
     },
     methods: {
+        // 获取园区列表
+        getGardenList() {
+            this.loading = true;
+            this.ajax
+                .post("/api/v1/adam/garden/list", {
+                    pageNum: 1,
+                    pageSize: 100,
+                    param: {},
+                })
+                .then((r) => {
+                    this.gardenList = r.data.map((i) => {
+                        i.showMenu = false;
+                        return i;
+                    });
+                    this.loading = false;
+                });
+        },
+        // 关闭园区列表
+        onCloseGardenList(done) {
+            done();
+        },
         tabClick(index) {
             this.activeTabIndex = index;
             this.$router.push(this.headTab[index].path);
@@ -59,16 +126,17 @@ export default {
     width: 320px;
 }
 .headBox {
+    width: calc(100% - 200px);
     display: flex;
     justify-content: space-between;
     align-items: center;
 }
 .btn {
     width: 94px;
-    height: 50px;
+    height: 45px;
     padding: 0 20px;
     position: absolute;
-    top: 10px;
+    top: 30px;
     right: 0;
     display: flex;
     cursor: pointer;
@@ -81,6 +149,112 @@ export default {
     i {
         display: inline-block;
         font-size: 18px;
+    }
+}
+.gardenList {
+    position: relative;
+    right: -20px;
+    .head {
+        padding: 10px 20px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.18);
+        border-radius: 100px 0px 0px 100px;
+        cursor: pointer;
+        > p {
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            color: #538dff;
+            i.erpanniu_jiantouxiangzuo_o {
+                display: inline-block;
+                transform: rotate(180deg);
+                font-size: 20px;
+                position: relative;
+                top: 1px;
+                margin-right: 5px;
+            }
+        }
+    }
+    .list {
+        width: 240px;
+        position: relative;
+        left: 12px;
+        margin-top: 5px;
+        border-top: 1px solid #e9e9e9;
+        .item {
+            width: 210px;
+            padding: 15px;
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            border: 1px solid #e9e9e9;
+            border-top: 0;
+            position: relative;
+            cursor: pointer;
+            img {
+                width: 65px;
+                height: 65px;
+                border-radius: 5px;
+            }
+            .c {
+                height: 60px;
+                margin-left: 10px;
+                display: flex;
+                justify-content: flex-start;
+                align-content: space-between;
+                flex-wrap: wrap;
+                p {
+                    width: 100%;
+                    color: #4c4c4c;
+                }
+                p:last-child {
+                    font-size: 12px;
+                    color: #538dff;
+                }
+            }
+            i {
+                display: inline-block;
+                position: absolute;
+                top: 20px;
+                cursor: pointer;
+                right: 10px;
+                color: #999999;
+                transform: rotate(90deg);
+            }
+            .menu {
+                width: 50px;
+                padding: 0 20px;
+                background: rgb(76, 76, 76);
+                position: absolute;
+                top: 50px;
+                right: 0px;
+                border-radius: 5px;
+                z-index: 2;
+                opacity: 0.92;
+                .menuPoint {
+                    width: 16px;
+                    height: 16px;
+                    position: absolute;
+                    top: -5px;
+                    right: 10px;
+                    background: rgb(76, 76, 76);
+                    transform: rotate(60deg);
+                }
+                p {
+                    cursor: pointer;
+                    font-size: 12px;
+                    text-align: center;
+                    color: #ffffff;
+                    padding: 10px 0;
+                    border-bottom: 1px solid #ffffff;
+                }
+                p:last-child {
+                    border: 0;
+                }
+            }
+        }
     }
 }
 </style>
