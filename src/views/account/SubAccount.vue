c86e83a5f309ac8114b55ea5f1ea94768f2482c2
Mins <xu_xiangbo@163.com>
2022-12-09 17:19:30 +0800

    clear views

--- src/views/account/SubAccount.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="subAccount">子账号</div>
-</template>
-
-<script lang="js">
-export default {
-    name: "subAccount",
-}
-</script>
--- src/views/ied.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="ied">智能设备</div>
-</template>
-
-<script>
-export default {
-    name: "ied",
-};
-</script>
